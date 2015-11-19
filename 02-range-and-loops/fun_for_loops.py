b6c20ac5b9a9e2c5c1920d13ac9eebbc04aafd84
lauramatchett <laura.m.matchett@gmail.com>
2015-11-18 21:17:17 -0800

    sum-nums!

---
+++ 02-range-and-loops/fun_for_loops.py
@@ -0,0 +1,28 @@
+#for i in range (10,-1, -1):
+#	if (i == 0):
+#		print "Blast off!"
+#	else:
+#		print i,		
+
+
+#fruits = ["apples", "oranges", "bananas"]
+
+#for f in range(5):
+	
+#	print fruits,
+
+
+
+
+def sum_nums(num):
+	sum = 0
+
+
+	for i in range(num):
+ 		sum = i + sum
+
+ 	return sum
+
+
+
+print sum_nums(10)
